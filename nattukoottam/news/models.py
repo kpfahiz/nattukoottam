a817e05a92f990c2dbd63fd81389df41b1244342
fahiz <fahizkppullara@gmail.com>
2024-05-05 12:57:28 +0530

    News Model

--- nattukoottam/news/models.py
+++ nattukoottam/news/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+    heading             = models.CharField(max_length=500)
+    title               = models.TextField()
+    date                = models.DateTimeField()
+    pic                 = models.ImageField(upload_to='News', default='ads.jpeg')
+    content             = models.TextField()
+
+    def __str__(self) -> str:
+        return self.heading
