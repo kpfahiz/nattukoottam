c9510410198989c779ac7dc480e6099e9a35127a
fahiz <fahizkppullara@gmail.com>
2024-05-05 09:09:45 +0530

    Setting

--- nattukoottam/nattukoottam/settings.py
+++ nattukoottam/nattukoottam/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #My Apps
+    'donor',
+    'patient',
+    'news',
+    'ads',
+
+    #Third Party
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
